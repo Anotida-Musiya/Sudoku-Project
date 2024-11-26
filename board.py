4a739cf0b0be45d7c78a6ef4f5845eb1339a50eb
AaronBezi <aaronbezi4@gmail.com>
2024-11-26 18:15:04 -0500

    Board Structure

--- board.py
+++ board.py
@@ -6,3 +6,36 @@ class Board:
         self.height = height
         self.screen = screen
         self.difficulty = difficulty
+
+    def draw(self):
+        pass
+
+    def select(self, row, col):
+        pass
+
+    def click(self, row, col):
+        pass
+
+    def clear(self):
+        pass
+
+    def sketch(self, value):
+        pass
+
+    def place_number(self, number):
+        pass
+
+    def reset_to_original(self):
+        pass
+
+    def is_full(self):
+        pass
+
+    def update_board(self):
+        pass
+
+    def find_empty(self):
+        pass
+
+    def check_board(self):
+        pass
